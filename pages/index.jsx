1f0ab4ef8cda11acfdc5f788d53ddcaa939b8acf
Parth khunt <parthkhunt2704@gmail.com>
2024-05-17 19:15:15 +0530

    API stop

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,12 +17,13 @@ function Home() {
   const [homeData, setHomeData] = useState([])
   const [bestSellingData, setBestSellingData] = useState([])
 
-  useEffect(() => {
-    (async () => {
-      axios.post("get-home-content").then(res => setHomeData(res?.data?.result))
-      axios.post("best-sellers").then(res => setBestSellingData(res?.data?.best_sellers))
-    })();
-  }, []); 
+  // useEffect(() => {
+  //   (async () => {
+  //     axios.post("get-home-content").then(res => setHomeData(res?.data?.result))
+  //     axios.post("best-sellers").then(res => setBestSellingData(res?.data?.best_sellers))
+  //   })();
+  // }, []); 
+
 
   return (
     <>
@@ -33,20 +34,20 @@ function Home() {
         image={homeData?.banner?.[1]?.image}
       />
       <Banner banner={homeData?.banner?.[1]} imgUrl={homeData?.image} />
-      {/* <FounderInfo
+      <FounderInfo
         heading={homeData?.home?.heading_1}
         subheading={homeData?.home?.sub_heading_1}
         sign={homeData?.home?.founder_image}
         name={homeData?.home?.founder_name}
-      /> */}
-      {/* <Collections
+      />
+      <Collections
         category1={homeData?.category_list?.[0]}
         category2={homeData?.category_list?.[1]}
-      /> */}
-      {/* <TradingCollections
+      />
+      <TradingCollections
         heading={homeData?.home?.heading_2}
         subheading={homeData?.home?.sub_heading_2}
-      /> */}
+      />
       <UpcomingProduct data={homeData?.recently_added} />
       <ContactUs
         heading={homeData?.home?.heading_3}
